20caf6de658704c0dcc3edea6f9df5fa75691fcd
Uma Manandhar <um_2003_8@hotmail.com>
2019-11-15 19:48:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,8 +17,8 @@ def long_planeteer_calls(calls)# code an argument here
   # Your code here
 end
 
-def find_the_cheese(cheese_types)
-  cheese_types.find {|cheese| cheese == "cheddar" || cheese == "gouda" || cheese == "camembert"}# code an argument here
+def find_the_cheese(array)
+  array.find {|cheese| cheese == "cheddar" || cheese == "gouda" || cheese == "camembert"}# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
